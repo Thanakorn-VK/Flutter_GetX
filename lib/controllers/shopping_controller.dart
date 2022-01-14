d6356dda61208e6bfb05121ff82e273ddd54690d
thanakon <thanakorn-vk@hotmail.com>
2022-01-14 14:15:04 +0700

    try catch

--- lib/controllers/shopping_controller.dart
+++ lib/controllers/shopping_controller.dart
@@ -10,15 +10,24 @@ class ShoppingController extends GetxController {
     super.onInit();
   }
   void fetchProducts() async { 
-    await Future.delayed(const Duration(seconds: 1));
-    var productResult = [
-      Product(1,'FirstProduct','abd','some description about product',25),
-      Product(2,'SecondProduct','abd','some description about product',14.5),
-      Product(3,'ThirdProduct','abd','some description about product',52),
-      Product(4,'FourthProduct','abd','some description about product',17.5),
-      Product(5,'FifthProduct','abd','some description about product',26.5),
-      Product(6,'SixthProduct','abd','some description about product',5.5)
-    ];
-    products.assignAll(productResult);
+    try{
+      await Future.delayed(const Duration(seconds: 1));
+      var productResult = [
+        Product(1,'FirstProduct','abd','some description about product',25),
+        Product(2,'SecondProduct','abd','some description about product',14.5),
+        Product(3,'ThirdProduct','abd','some description about product',52),
+        Product(4,'FourthProduct','abd','some description about product',17.5),
+        Product(5,'FifthProduct','abd','some description about product',26.5),
+        Product(6,'SixthProduct','abd','some description about product',5.5)
+      ];
+      if(productResult.isEmpty){
+        throw Exception('Products is empty');
+      }
+      products.assignAll(productResult);
+    }
+    catch(e){
+      // ignore: avoid_print
+      print(e);
+    }
   }
 }
